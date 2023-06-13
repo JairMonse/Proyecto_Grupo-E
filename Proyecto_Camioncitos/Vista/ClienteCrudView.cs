0c63e6bf8c7cbc9f2165b6dba617da13d48e7f8a
JMTM <69547208+JairMonse@users.noreply.github.com>
2023-06-13 00:58:54 -0500

    Add files via upload

---
+++ Proyecto_Camioncitos/Vista/ClienteCrudView.cs
@@ -0,0 +1,44 @@
+﻿using ProyectoCamioncitos.Controlador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCamioncitos
+{
+    //Vista CRUD Cliente
+    public partial class ClienteCrudView : Form
+    {
+        public ClienteCrudView()
+        {
+            InitializeComponent();
+            //Vista a Controlador
+            ClienteCrudController ctrl = new ClienteCrudController(this);
+        }
+
+        private void gbClienteDatos_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
