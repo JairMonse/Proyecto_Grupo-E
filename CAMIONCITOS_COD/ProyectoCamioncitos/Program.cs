2cbacd4c738987410f4570b43d3797747a9d23fa
Erick <115914523+erickbcr7@users.noreply.github.com>
2023-07-24 22:46:34 -0500

    Add files via upload

---
+++ CAMIONCITOS_COD/ProyectoCamioncitos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCamioncitos
+{
+    class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LoginView main = new LoginView();
+            main.FormClosed += MainForm_Closed;
+            main.Show();
+            Application.Run();
+        }
+        //Cuando detecta que no hay ninguna ventana abierta cierra el programa
+        private static void MainForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            ((Form)sender).FormClosed -= MainForm_Closed;
+
+            if (Application.OpenForms.Count == 0)
+            {
+                Application.ExitThread();
+            }
+            else
+            {
+                Application.OpenForms[0].FormClosed += MainForm_Closed;
+            }
+        }
+    }
+}
